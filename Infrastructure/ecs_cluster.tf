a53e0e4c90566d6c51c4b4768b3dcaede783ac8c
Pranshu <jain.pranshu98@gmail.com>
2024-07-24 22:32:28 +0530

    ECS Cluster

---
+++ Infrastructure/ecs_cluster.tf
@@ -0,0 +1,23 @@
+resource "aws_ecs_cluster" "main" {
+  name = "microservices-cluster"
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionPolicy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
